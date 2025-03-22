a128dd0a9503e80b93ac13db5fa017bdaf723fcc
Abdallah-Mushtaha <abood.r.mushtaha@gmail.com>
2025-03-22 03:34:35 +0200

    Modify layout.js in Folder  sabaa ?  :|

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body>
         <HomeLayout>{children}</HomeLayout>
       </body>
