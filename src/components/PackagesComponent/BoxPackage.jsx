a62df28a96e2945722cc552625d153a6d8aa507e
karamzomlot <zomlutk@gmail.com>
2025-12-17 17:33:57 +0200

    feat: add titles for some pages and some fixes in style

--- src/components/PackagesComponent/BoxPackage.jsx
+++ src/components/PackagesComponent/BoxPackage.jsx
@@ -1,85 +1,69 @@
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 let Data = [
   {
-    ouetricon: "/img/Packages/Shape.svg",
+    ouetricon: '/img/Packages/Shape.svg',
     h4: ' باقة الإطلاق الأولي "MVP" ',
-    h2: " 15,000 ريال سعودي ",
-    p: " نقدم لك حلولًا سريعة وفعّالة لتنفيذ منتجك الأولي (MVP) بأسعار تنافسية وجودة عالية. ابدأ الآن واختبر فكرتك في السوق بأقل وقت وتكلفة! ",
-    Innericon: "/img/ProjectsDetails/elements.svg",
+    h2: ' 15,000 ريال سعودي ',
+    p: ' نقدم لك حلولًا سريعة وفعّالة لتنفيذ منتجك الأولي (MVP) بأسعار تنافسية وجودة عالية. ابدأ الآن واختبر فكرتك في السوق بأقل وقت وتكلفة! ',
+    Innericon: '/img/ProjectsDetails/elements.svg',
     subTitle: [
-      " تحليل متطلبات المشروع وتخطيط أولي. ",
-      " تصميم واجهات مستخدم (UI/UX) لأهم ٨-١٢ شاشة. ",
-      " تطوير واجهة المستخدم والخلفية لتطبيق MVP. ",
-      " تكامل مع واجهات برمجية (APIs). ",
-      " اختبار الجودة وإصلاح الأخطاء قبل الإطلاق. ",
-      "نشر التطبيق/الموقع في بيئة تجريبية.",
-      "نشر التطبيق/الموقع في بيئة تجريبية.",
+      ' تحليل متطلبات المشروع وتخطيط أولي. ',
+      ' تصميم واجهات مستخدم (UI/UX) لأهم ٨-١٢ شاشة. ',
+      ' تطوير واجهة المستخدم والخلفية لتطبيق MVP. ',
+      ' تكامل مع واجهات برمجية (APIs). ',
+      ' اختبار الجودة وإصلاح الأخطاء قبل الإطلاق. ',
+      'نشر التطبيق/الموقع في بيئة تجريبية.',
+      'نشر التطبيق/الموقع في بيئة تجريبية.'
     ],
-    btnContent: "ابداء الان",
+    btnContent: 'ابدأ الآن'
   },
   {
-    ouetricon: "/img/Packages/twiceShape.svg",
-    h4: "باقة المشاريع الكبيرة أو فرص التعاون ",
-    h2: " السعر حسب التفاصيل",
-    p: "سواء كنت تبحث عن تطوير مشروع ضخم أو تعاون طويل الأمد، نحن هنا لتحقيق أهدافك. تواصل معنا اليوم لمناقشة تفاصيل مشروعك!",
-    Innericon: "/img/Packages/InnerIcon.svg",
+    ouetricon: '/img/Packages/twiceShape.svg',
+    h4: 'باقة المشاريع الكبيرة أو فرص التعاون ',
+    h2: ' السعر حسب التفاصيل',
+    p: 'سواء كنت تبحث عن تطوير مشروع ضخم أو تعاون طويل الأمد، نحن هنا لتحقيق أهدافك. تواصل معنا اليوم لمناقشة تفاصيل مشروعك!',
+    Innericon: '/img/Packages/InnerIcon.svg',
     subTitle: [
-      " دراسة جدوى فنية وتخطيط معمق للمشروع. ",
-      " تصميم واجهات مستخدم مخصصة لجميع الشاشات. ",
-      " تطوير كامل للتطبيق (واجهة المستخدم، الخلفية، وقاعدة البيانات). ",
-      " تكامل مع أدوات وواجهات برمجية متقدمة. ",
-      " اختبار شامل (وظيفي، أداء، وأمان). ",
-      " نشر التطبيق في بيئة إنتاجية.  ",
-      " دعم فني وصيانة لمدة 3-6 أشهر بعد الإطلاق. ",
-      " تحديثات وتحسينات دورية حسب الطلب. ",
+      ' دراسة جدوى فنية وتخطيط معمق للمشروع. ',
+      ' تصميم واجهات مستخدم مخصصة لجميع الشاشات. ',
+      ' تطوير كامل للتطبيق (واجهة المستخدم، الخلفية، وقاعدة البيانات). ',
+      ' تكامل مع أدوات وواجهات برمجية متقدمة. ',
+      ' اختبار شامل (وظيفي، أداء، وأمان). ',
+      ' نشر التطبيق في بيئة إنتاجية.  ',
+      ' دعم فني وصيانة لمدة 3-6 أشهر بعد الإطلاق. ',
+      ' تحديثات وتحسينات دورية حسب الطلب. '
     ],
-    btnContent: " تواصل معنا ",
-  },
+    btnContent: ' تواصل معنا '
+  }
 ];
 export function BoxPackage() {
   return (
-    <section className="boxPackages mt-5 ">
-      <div className="container">
-        <div className="WrapperBoxOuter d-flex-column  d-md-flex d-xl-flex justify-content-center align-items-center gap-5">
+    <section className='boxPackages mt-5 '>
+      <div className='container'>
+        <div className='WrapperBoxOuter d-flex-column  d-md-flex d-xl-flex justify-content-center align-items-start gap-5'>
           {Data.map((outeritem, index) => {
             return (
               <div
                 key={index}
-                className={`boxWrapper ${index === 1 ? "Active " : ""} `}
+                className={`boxWrapper ${index === 1 ? 'Active ' : ''} `}
                 // data-aos={index % 2 === 0 ? "fade-right" : "fade-left"}
-                data-aos="zoom-in"
+                data-aos='zoom-in'
               >
-                <div className="container">
+                <div className='container'>
                   <small>
-                    <img loading="lazy"
-                      src={outeritem.ouetricon}
-                      alt="Icon"
-                      onError={(e) => {
-                        e.currentTarget.src = "../img/LoagingState.png";
-                        e.currentTarget.style.objectFit = "contain";
-                      }}
-                    />
+                    <Image loading='lazy' width={16} height={33} className='w-auto' src={outeritem.ouetricon} alt='Icon' />
                   </small>
-                  <h4 className="mt-4">{outeritem.h4}</h4>
+                  <h4 className='mt-4'>{outeritem.h4}</h4>
                   <h2>{outeritem.h2}</h2>
-                  <p className="mb-5">{outeritem.p} </p>
-                  <div className="box-innerfeatures">
+                  <p className='mb-5'>{outeritem.p} </p>
+                  <div className='box-innerfeatures'>
                     {outeritem.subTitle.map((item, index) => {
                       return (
-                        <div
-                          key={index}
-                          className="innerfeatures d-flex justify-content-center align-items-center gap-3 my-3"
-                        >
-                          <img loading="lazy"
-                            src={outeritem.Innericon}
-                            alt="check"
-                            onError={(e) => {
-                              e.currentTarget.src = "../img/LoagingState.png";
-                              e.currentTarget.style.objectFit = "contain";
-                            }}
-                          />
-                          <p key={index} className="inner_P_Tag mb-0">
+                        <div key={index} className='innerfeatures d-flex justify-content-center align-items-center gap-3 my-3'>
+                          <Image loading='lazy' src={outeritem.Innericon} width={20} height={20} alt='check' />
+                          <p key={index} className='inner_P_Tag mb-0'>
                             {item}
                           </p>
                         </div>
@@ -88,11 +72,8 @@ export function BoxPackage() {
                   </div>
                 </div>
 
-                <div className="Wrapper ">
-                  <Link
-                    href="/contact-us"
-                    className="d-flex justify-content-center"
-                  >
+                <div className='Wrapper '>
+                  <Link href='/contact-us' className='d-flex justify-content-center'>
                     <button>{outeritem.btnContent}</button>
                   </Link>
                 </div>
