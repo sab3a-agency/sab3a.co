4b321d283527694879e01b0820af49d0dae28ce4
karamzomlot <dev.karamzomlot@gmail.com>
2025-12-27 13:08:02 +0200

    refactor: make files extension .jsx instead of .js

---
+++ src/app/(pages)/projects/page.jsx
@@ -0,0 +1,13 @@
+import Hero_Service from '@/components/ServeceComponent/hero_Service';
+import LetsDoit from '@/components/LetsDoit';
+import Section2 from '@/components/ServeceComponent/section2';
+
+export default function ServicesPage() {
+  return (
+    <>
+      <Hero_Service />
+      <Section2 />
+      <LetsDoit />
+    </>
+  );
+}
