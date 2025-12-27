7c419bea0fb7eff369850334f78a1917cdaca359
karamzomlot <dev.karamzomlot@gmail.com>
2025-12-27 13:23:05 +0200

    refactor: rename folder to projects

---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import SkeletonBox from '../SkeletonBox';
+import ErrorRequest from '../ErrorRequest';
+
+export default function Projects({ Data }) {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (Data && Array.isArray(Data) && Data.length === 2) {
+      setProjects(Data);
+      setLoading(false);
+      return;
+    }
+
+    async function fetchProjects() {
+      try {
+        const res = await fetch('/api/projects');
+        if (!res.ok) throw new Error('Network response was not ok');
+        const data = await res.json();
+
+        setProjects(data.data.items);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProjects();
+  }, [Data]);
+
+  if (loading) {
+    return (
+      <div className='projectsWrapper mt-5' data-aos='fade-up' data-aos-duration='1000'>
+        <div className='row justify-content-between flex-wrap-wrap'>
+          {[...Array(2)].map((_, i) => (
+            <SkeletonBox key={i} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  if (error) return <ErrorRequest />;
+
+  return (
+    <div className='projectsWrapper mt-5' data-aos='fade-up' data-aos-duration='1000'>
+      <div className='row justify-content-between flex-wrap-wrap'>
+        {projects.map((project) => (
+          <div key={project.id} className='box col-12 col-md-6'>
+            <Link href={`/projects/${project.id}`} className='imgWrapper' style={{ cursor: 'pointer' }}>
+              <img
+                src={project.cover_image?.replace(/\\/g, '/')}
+                alt={project.title}
+                loading='eager'
+                onError={(e) => {
+                  e.currentTarget.src = '../img/LoagingState.png';
+                  e.currentTarget.style.objectFit = 'contain';
+                }}
+              />
+            </Link>
+            <div className='info mt-2 p-3'>
+              <h2 className='mb-4'>{project.title}</h2>
+              <p className='my-3'>{project.sub_title}</p>
+            </div>
+            <div className='anchors d-flex gap-3 container'>
+              <a href='#'>{project.category}</a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/projects/section2.jsx
@@ -0,0 +1,39 @@
+'use client';
+import { PartnersMarquee } from './marquees';
+
+const Data = {
+  title: 'شريكك في',
+  span: 'النجاح الرقمي ',
+  innerTitle: 'في',
+  innerSpan: 'سبعة',
+  innerTitleEnd: '، نؤمن بأن التصميم الاستثنائي يتجاوز الجماليات.',
+  discriptions: ' إنه يشمل الوظائف والقدرة على تحقيق الغرض المقصود. فريقنا الم dedicated هو مدفوع بهدف إنشاء علامات تجارية تُحدث تأثيرًا ذا  مغزى.'
+};
+
+export default function Section2() {
+  return (
+    <section className='Section2 mt-80 '>
+      <div className='container'>
+        <div className='adv '>
+          <div className='container d-flex-column d-md-flex  justify-content-between align-items-center '>
+            <div className='info ' data-aos='zoom-in'>
+              <h2 className=' d-flex gap-2 justify-content-start'>
+                {Data.title}
+                <span>{Data.span}</span>
+              </h2>
+            </div>
+            <div className='info   d-flex flex-column gap-5' data-aos='zoom-in'>
+              <p>
+                {Data.innerTitle}
+                <span>{Data.innerSpan}</span>
+                {Data.innerTitleEnd}
+              </p>
+              <p>{Data.discriptions}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <PartnersMarquee />
+    </section>
+  );
+}
