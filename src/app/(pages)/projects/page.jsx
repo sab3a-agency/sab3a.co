4ac046e4cf1499623d053bd957fe641e08fd9ffb
karamzomlot <dev.karamzomlot@gmail.com>
2025-12-31 13:53:40 +0200

    feat: enhance projects section

--- src/app/(pages)/projects/page.jsx
+++ src/app/(pages)/projects/page.jsx
@@ -1,11 +1,18 @@
 import ProjectsHeader from '@/components/projects/projects-header';
 import LetsDoit from '@/components/LetsDoit';
 import PartnersSection from '@/components/projects/partners-section';
+import Projects from '@/components/projects/projects';
 
-export default function ServicesPage() {
+export default async function ProjectsPage() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/projects`, {
+    cache: 'no-store'
+  });
+  const { data } = await res.json();
+
   return (
     <>
-      <ProjectsHeader />
+      <ProjectsHeader data={data.section} />
+      <Projects projects={data.items} />
       <PartnersSection />
       <LetsDoit />
     </>
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,31 +1,30 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default async function Projects() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/projects`, {
-    cache: 'no-store'
-  });
-  const {
-    data: { items: projects }
-  } = await res.json();
-
+export default function Projects({ projects }) {
   return (
-    <div className='projectsWrapper mt-5' data-aos='fade-up' data-aos-duration='1000'>
-      <div className='row justify-content-between flex-wrap-wrap'>
-        {projects.map((project) => (
-          <div key={project.id} className='box col-12 col-md-6'>
-            <Link href={`/projects/${project.id}`} className='imgWrapper'>
-              <Image src={project.cover_image?.replace(/\\/g, '/')} alt={project.title} loading='eager' width={608} height={500} />
-            </Link>
-            <div className='info mt-2 p-3'>
-              <h2 className='mb-4'>{project.title}</h2>
-              <p className='my-3'>{project.sub_title}</p>
-            </div>
-            <div className='anchors d-flex gap-3 container'>
-              <Link href='#'>{project.category}</Link>
-            </div>
+    <div className='border-bottom border-gray'>
+      <div className='container'>
+        <div className='projectsWrapper mt-5' data-aos='fade-up' data-aos-duration='1000'>
+          <div className='row justify-content-between flex-wrap-wrap '>
+            {projects.map((project) => (
+              <div key={project.id} className='box col-12 col-md-6 px-3'>
+                <Link href={`/projects/${project.id}`} className='imgWrapper d-inline-block w-100'>
+                  <Image src={project.cover_image} alt={project.title} loading='eager' width={608} height={500} />
+                </Link>
+                <div className='info mt-2 p-3'>
+                  <Link href={`/projects/${project.id}`}>
+                    <h2 className='mb-4'>{project.title}</h2>
+                  </Link>
+                  <p className='my-3'>{project.sub_title}</p>
+                </div>
+                <div className='anchors d-flex gap-3 container'>
+                  <span>{project.category}</span>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
